7724a56fde38f20952a550bcc2d4ef4336fd6010
Dinh Long <dinhlong.hoo@gmail.com>
2017-07-09 08:24:51 -0700

    the-hero-editor

--- src/app/app.component.js
+++ src/app/app.component.js
@@ -6,16 +6,26 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
 var core_1 = require("@angular/core");
+var Hero = (function () {
+    function Hero() {
+    }
+    return Hero;
+}());
+exports.Hero = Hero;
 var AppComponent = (function () {
     function AppComponent() {
-        this.name = 'Angular';
+        this.title = 'Tour of Heroes';
+        this.hero = {
+            id: 1,
+            name: 'Windstorm'
+        };
     }
     return AppComponent;
 }());
 AppComponent = __decorate([
     core_1.Component({
         selector: 'my-app',
-        template: "<h1>Hello {{name}}</h1>",
+        template: "\n  \t<h1>{{title}}</h1>\n  \t<h2>{{hero.name}} detail!</h2>\n  \t<div><label>id: </label>{{hero.id}}</div>\n  \t<div>\n  \t\t<input [(ngModel)]=\"hero.name\" type=\"text\" placeholder=\"name\"/>\n  \t</div>\n  ",
     })
 ], AppComponent);
 exports.AppComponent = AppComponent;
